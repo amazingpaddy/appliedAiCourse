c0ffdc927e617bf4695551034979ae37a02d17a6
Paddy <padmanabhan5789@gmail.com>
2022-03-08 21:58:02 -0500

    Learnings of LinearAlgebra, Calculas and Algos

--- Learning Python/projects/yamlParser.py
+++
@@ -1,7 +0,0 @@
-import yaml as yml
-
-with open("D:\\Paddy\\input.txt", 'r', encoding='utf-8') as configFile:
-    dataList = configFile.readlines()
-    for sentence in dataList:
-        result = sentence.replace(" ", "")
-        print(result)
