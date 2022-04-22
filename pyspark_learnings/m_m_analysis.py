844c8663346a8ca4ed2c6e64bac7f3e416a8583f
Paddy <padmanabhan5789@gmail.com>
2022-04-22 13:26:26 -0400

    Pyspark learnings

--- pyspark_learnings/m_m_analysis.py
+++ pyspark_learnings/m_m_analysis.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
 
 
 def start():
@@ -11,7 +12,20 @@ def start():
     data.printSchema()
     data.show()
     data.where(data.age > 28).show()
-    data.rdd.map(lambda r: print(type(r)))
+
+    # We can define the schema like below.
+    schema = StructType([StructField("author", StringType(), False), StructField("title", StringType(), False),
+                         StructField("pages", IntegerType(), False)])
+
+    #We can define the schema like below as well
+    schema_ddl = "author STRING, title STRING, pages INT"
+
+    new_data = [("Paddy", "Ponniyin Selvan", 1000), ("Gopi", "Sivakamiyin Sabatham", 2000)]
+    new_df = spark.createDataFrame(new_data, schema)
+    new_df_ddl_schema = spark.createDataFrame(new_data, schema_ddl)
+    new_df.show()
+    new_df_ddl_schema.show()
+
 
 
 if __name__ == "__main__":
